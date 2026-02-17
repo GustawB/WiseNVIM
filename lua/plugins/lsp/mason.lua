90259c44b278e9b7f3b970ad05e40a9fe3e50ad6
GustawB <gustaw.blachowski@gmail.com>
2026-02-17 19:35:59 +0100

    Move cmp-nvim-lsp to mason

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -3,12 +3,15 @@ return {
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "hrsh7th/cmp-nvim-lsp",
     },
 
     config = function()
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
         local mason_tool_installer = require("mason-tool-installer")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp.default_capabilities()
 
         mason.setup({
             ui = {
@@ -41,5 +44,12 @@ return {
                 "markdownlint",
             },
         })
+
+        local default_settings_servers = mason_lspconfig.get_installed_servers()
+        for _, v in ipairs(default_settings_servers) do
+            vim.lsp.config(v, {
+                capabilities = capabilities,
+            })
+        end
     end,
 }
