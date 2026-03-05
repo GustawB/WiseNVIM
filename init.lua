60374f47d6059a924b932d0c3b7a1a1ae7ca871d
GustawB <gustaw.blachowski@gmail.com>
2026-03-05 21:25:34 +0100

    Remappings for Telescope; pretty hover

--- init.lua
+++ init.lua
@@ -6,16 +6,19 @@ vim.g.loaded_python3_provider = 0
 vim.o.number = true
 
 local map = vim.keymap.set
-
+vim.keymap.set("n", "K", function()
+    require("hover").open()
+end, { desc = "hover.nvim (open)" })
 map("n", "<leader>e", require("oil").open_float, { desc = "Open Oil floating window" })
 
 local builtin = require("telescope.builtin")
-map("n", "<leader>tf", builtin.find_files, { desc = "Telescope find files" })
-map("n", "<leader>tr", builtin.lsp_references, { desc = "Telescope find refs" })
-map("n", "<leader>tg", builtin.live_grep, { desc = "Telescope live grep" })
-map("n", "<leader>ti", builtin.lsp_implementations, { desc = "Telescope implementations" })
-map("n", "<leader>td", builtin.lsp_definitions, { desc = "Telescope definitions" })
-map("n", "<leader>tt", builtin.lsp_type_definitions, { desc = "Telescope type_definitions" })
+map("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
+map("n", "<leader>r", builtin.lsp_references, { desc = "Telescope find refs" })
+map("n", "<leader>l", builtin.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>g", builtin.grep_string, { desc = "Telescope grep string" })
+map("n", "<leader>i", builtin.lsp_implementations, { desc = "Telescope implementations" })
+map("n", "<leader>d", builtin.lsp_definitions, { desc = "Telescope definitions" })
+map("n", "<leader>t", builtin.lsp_type_definitions, { desc = "Telescope type_definitions" })
 
 map("n", "<C-S>", function()
     vim.ui.input({ prompt = "Session Name: " }, function(input)
